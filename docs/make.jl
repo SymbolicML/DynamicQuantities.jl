d2aa54a6ffb468234c73078b2648f203cb1ff451
MilesCranmer <miles.cranmer@gmail.com>
2023-06-06 00:25:55 -0400

    Get docs to read from README

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,28 @@ using Documenter
 
 DocMeta.setdocmeta!(DynamicUnits, :DocTestSetup, :(using DynamicUnits); recursive=true)
 
+readme = open(dirname(@__FILE__) * "/../README.md") do io
+    read(io, String)
+end
+
+# We replace every instance of <img src="IMAGE" ...> with ![](IMAGE).
+readme = replace(readme, r"<img src=\"([^\"]+)\"[^>]+>.*" => s"![](\1)")
+
+# Then, we remove any line with "<div" on it:
+readme = replace(readme, r"<[/]?div.*" => s"")
+
+# Finally, we read in file docs/src/index_base.md:
+index_base = open(dirname(@__FILE__) * "/src/index_base.md") do io
+    read(io, String)
+end
+
+# And then we create "/src/index.md":
+open(dirname(@__FILE__) * "/src/index.md", "w") do io
+    write(io, readme)
+    write(io, "\n")
+    write(io, index_base)
+end
+
 makedocs(;
     modules=[DynamicUnits],
     authors="MilesCranmer <miles.cranmer@gmail.com> and contributors",
