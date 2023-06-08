0e13532e4edd5322da9b0628721021f1c1f31e9c
MilesCranmer <miles.cranmer@gmail.com>
2023-06-08 03:00:49 -0400

    Use SafeInt8 in cached rational

--- src/cached_rational.jl
+++ src/cached_rational.jl
@@ -1,41 +1,45 @@
+import SaferIntegers: SafeInt8
+
+const INT_TYPE = SafeInt8
+
 const gcd_table =
     Tuple(
         Tuple(
             if j == 0
-                (Int8(0), Int8(0))
+                (INT_TYPE(0), INT_TYPE(0))
             else
-                let (out1, out2) = Base.divgcd(Int8(i), Int8(j))
-                    (Int8(out1), Int8(out2))
+                let (out1, out2) = Base.divgcd(i, j)
+                    (INT_TYPE(out1), INT_TYPE(out2))
                 end
             end
-            for i = (typemin(Int8)+1):typemax(Int8)
+            for i = (Int(typemin(INT_TYPE))+1):Int(typemax(INT_TYPE))
         )
-        for j = (typemin(Int8)+1):typemax(Int8)
+        for j = (Int(typemin(INT_TYPE))+1):Int(typemax(INT_TYPE))
     );
 
-# Pirate divgcd to speed it up for Int8
-function cached_divgcd(x::Int8, y::Int8)
-    @inbounds gcd_table[y+128][x+128]
+# Pirate divgcd to speed it up for INT_TYPE
+function cached_divgcd(x::INT_TYPE, y::INT_TYPE)
+    @inbounds gcd_table[Int(y)+128][Int(x)+128]
 end
 
 """Cached rational number"""
 struct CRational{T} <: Real
     num::T
     den::T
-    global unsafe_rational(num::Integer, den::Integer) = new{Int8}(Int8(num), Int8(den))
+    global unsafe_rational(num::Integer, den::Integer) = new{INT_TYPE}(INT_TYPE(num), INT_TYPE(den))
 end
 
-CRational(num::Int8, den::Int8) =
+CRational(num::INT_TYPE, den::INT_TYPE) =
     let (num, den) = cached_divgcd(num, den)
         return unsafe_rational(num, den)
     end
-CRational(n::Int8) = unsafe_rational(n, one(n))
-CRational(n::Integer) = CRational(Int8(n))
-CRational(r::Rational{Int8}) = unsafe_rational(r.num, r.den)
-CRational(r::Rational) = CRational(Int8(r.num), Int8(r.den))
+CRational(n::INT_TYPE) = unsafe_rational(n, one(n))
+CRational(n::Integer) = CRational(INT_TYPE(n))
+CRational(r::Rational{INT_TYPE}) = unsafe_rational(r.num, r.den)
+CRational(r::Rational) = CRational(INT_TYPE(r.num), INT_TYPE(r.den))
 
-CRational{Int8}(n::Integer) = CRational(n)
-CRational{Int8}(r::Rational) = CRational(r)
+CRational{INT_TYPE}(n::Integer) = CRational(n)
+CRational{INT_TYPE}(r::Rational) = CRational(r)
 
 Base.:*(x::CRational, y::CRational) = CRational(x.num * y.num, x.den * y.den)
 Base.:+(x::CRational, y::CRational) =
@@ -46,7 +50,7 @@ Base.:-(x::CRational) = unsafe_rational(-x.num, x.den)
 Base.:-(x::CRational, y::CRational) = x + (-y)
 Base.:(==)(x::CRational, y::CRational) = (x.den == y.den) & (x.num == y.num)
 Base.iszero(x::CRational) = iszero(x.num)
-Base.isinteger(x::CRational) = x.den == one(Int8)
+Base.isinteger(x::CRational) = x.den == one(INT_TYPE)
 Base.convert(::Type{RI}, x::CRational) where {RI<:Rational} = RI(x.num, x.den)
 Base.string(x::CRational) = string(convert(Rational, x))
 Base.promote(x::CRational, y) = promote(convert(Rational, x), y)
