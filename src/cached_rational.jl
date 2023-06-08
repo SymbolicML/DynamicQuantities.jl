da52b1e7d3a19fdf93f8b151ab0f5f0395f9e086
MilesCranmer <miles.cranmer@gmail.com>
2023-06-08 00:55:26 -0400

    Fix divgcd and assume @inbounds

--- src/cached_rational.jl
+++ src/cached_rational.jl
@@ -1,13 +1,19 @@
 import Base: divgcd
 
-function _divgcd(x::Int8, y::Int8)
-    iszero(y) && return (zero(Int8), zero(Int8))
-    g = gcd(x, y)
-    div(x, g), div(y, g)
-end
+const gcd_table =
+    Tuple(
+        Tuple(
+            if j == 0
+                (Rational{Int8}(0), Rational{Int8}(0))
+            else
+                Rational{Int8}.(divgcd(Int16(i), Int16(j)))
+            end
+            for i = (typemin(Int8)+1):typemax(Int8)
+        )
+        for j = (typemin(Int8)+1):typemax(Int8)
+    );
 
-const gcd_table = Tuple(Tuple(_divgcd(Int8(i), Int8(j)) for i = (typemin(Int8)+1):typemax(Int8)) for j = (typemin(Int8)+1):typemax(Int8));
-
+# Pirate divgcd to speed it up for Int8
 function divgcd(x::Int8, y::Int8)
-    gcd_table[x+2-typemin(Int8)][y+2-typemin(Int8)]
+    @inbounds gcd_table[y+128][x+128]
 end
