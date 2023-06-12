cbe5754c3431c9fd87b8329b1e103e3a79216213
MilesCranmer <miles.cranmer@gmail.com>
2023-06-11 21:22:06 -0400

    Export `uparse` and `u_str`

--- src/DynamicQuantities.jl
+++ src/DynamicQuantities.jl
@@ -2,13 +2,16 @@ module DynamicQuantities
 
 export Quantity, Dimensions, DimensionError, ustrip, dimension, valid
 export ulength, umass, utime, ucurrent, utemperature, uluminosity, uamount
+export uparse, @u_str
 
 include("fixed_rational.jl")
 include("types.jl")
 include("utils.jl")
 include("math.jl")
+include("units.jl")
 
 import Requires: @init, @require
+import .Units: uparse, @u_str
 
 if !isdefined(Base, :get_extension)
     @init @require Unitful = "1986cc42-f94f-5a68-af5c-568840ba703d" include("../ext/DynamicQuantitiesUnitfulExt.jl")
