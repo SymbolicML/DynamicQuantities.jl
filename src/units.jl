34433664acb44492b55287d03f3de5a7f627a1d1
MilesCranmer <miles.cranmer@gmail.com>
2023-06-29 13:41:01 -0400

    Create UnitDimensions struct for pretty-printing

--- src/units.jl
+++ src/units.jl
@@ -5,13 +5,27 @@ export uparse, @u_str
 import ..DEFAULT_DIM_TYPE
 import ..DEFAULT_VALUE_TYPE
 import ..Quantity
+import ..AbstractDimensions
+
+const _UNIT_SYMBOLS = Symbol[]
 
 @assert DEFAULT_VALUE_TYPE == Float64 "`units.jl` must be updated to support a different default value type."
 
+macro register_unit(name, value)
+    return esc(_register_unit(name, value))
+end
+
 macro add_prefixes(base_unit, prefixes)
     @assert prefixes.head == :tuple
-    expr = _add_prefixes(base_unit, prefixes.args)
-    return expr |> esc
+    return esc(_add_prefixes(base_unit, prefixes.args))
+end
+
+function _register_unit(name::Symbol, value)
+    s = string(name)
+    return quote
+        const $name = $value
+        push!(_UNIT_SYMBOLS, Symbol($s))
+    end
 end
 
 function _add_prefixes(base_unit::Symbol, prefixes)
@@ -23,26 +37,19 @@ function _add_prefixes(base_unit::Symbol, prefixes)
     for (prefix, value) in zip(keys(all_prefixes), values(all_prefixes))
         prefix in prefixes || continue
         new_unit = Symbol(prefix, base_unit)
-        push!(expr.args, :(const $new_unit = $value * $base_unit))
+        push!(expr.args, _register_unit(new_unit, :($value * $base_unit)))
     end
     return expr
 end
 
 # SI base units
-"Length in meters. Available variants: `fm`, `pm`, `nm`, `μm` (/`um`), `cm`, `dm`, `mm`, `km`, `Mm`, `Gm`."
-const m = Quantity(1.0, length=1)
-"Mass in grams. Available variants: `μg` (/`ug`), `mg`, `kg`."
-const g = Quantity(1e-3, mass=1)
-"Time in seconds. Available variants: `fs`, `ps`, `ns`, `μs` (/`us`), `ms`, `min`, `h` (/`hr`), `day`, `yr`, `kyr`, `Myr`, `Gyr`."
-const s = Quantity(1.0, time=1)
-"Current in Amperes. Available variants: `nA`, `μA` (/`uA`), `mA`, `kA`."
-const A = Quantity(1.0, current=1)
-"Temperature in Kelvin. Available variant: `mK`."
-const K = Quantity(1.0, temperature=1)
-"Luminosity in candela. Available variant: `mcd`."
-const cd = Quantity(1.0, luminosity=1)
-"Amount in moles. Available variant: `mmol`."
-const mol = Quantity(1.0, amount=1)
+@register_unit m Quantity(1.0, length=1)
+@register_unit g Quantity(1e-3, mass=1)
+@register_unit s Quantity(1.0, time=1)
+@register_unit A Quantity(1.0, current=1)
+@register_unit K Quantity(1.0, temperature=1)
+@register_unit cd Quantity(1.0, luminosity=1)
+@register_unit mol Quantity(1.0, amount=1)
 
 @add_prefixes m (f, p, n, μ, u, c, d, m, k, M, G)
 @add_prefixes g (μ, u, m, k)
@@ -53,27 +60,17 @@ const mol = Quantity(1.0, amount=1)
 @add_prefixes mol (m,)
 
 # SI derived units
-"Frequency in Hertz. Available variants: `kHz`, `MHz`, `GHz`."
-const Hz = inv(s)
-"Force in Newtons."
-const N = kg * m / s^2
-"Pressure in Pascals. Available variant: `kPa`."
-const Pa = N / m^2
-"Energy in Joules. Available variant: `kJ`."
-const J = N * m
-"Power in Watts. Available variants: `kW`, `MW`, `GW`."
-const W = J / s
-"Charge in Coulombs."
-const C = A * s
-"Voltage in Volts. Available variants: `kV`, `MV`, `GV`."
-const V = W / A
-"Capacitance in Farads."
-const F = C / V
-"Resistance in Ohms. Available variant: `mΩ`. Also available is ASCII `ohm` (with variant `mohm`)."
-const Ω = V / A
-const ohm = Ω
-"Magnetic flux density in Teslas."
-const T = N / (A * m)
+@register_unit Hz inv(s)
+@register_unit N kg * m / s^2
+@register_unit Pa N / m^2
+@register_unit J N * m
+@register_unit W J / s
+@register_unit C A * s
+@register_unit V W / A
+@register_unit F C / V
+@register_unit Ω V / A
+@register_unit ohm Ω
+@register_unit T N / (A * m)
 
 @add_prefixes Hz (k, M, G)
 @add_prefixes N ()
@@ -89,11 +86,11 @@ const T = N / (A * m)
 
 # Common assorted units
 ## Time
-const min = 60 * s
-const h = 60 * min
-const hr = h
-const day = 24 * h
-const yr = 365.25 * day
+@register_unit min 60 * s
+@register_unit h 60 * min
+@register_unit hr h
+@register_unit day 24 * h
+@register_unit yr 365.25 * day
 
 @add_prefixes min ()
 @add_prefixes h ()
@@ -102,14 +99,12 @@ const yr = 365.25 * day
 @add_prefixes yr (k, M, G)
 
 ## Volume
-"Volume in liters. Available variants: `mL`, `dL`."
-const L = dm^3
+@register_unit L dm^3
 
 @add_prefixes L (m, d)
 
 ## Pressure
-"Pressure in bars."
-const bar = 100 * kPa
+@register_unit bar 100 * kPa
 
 @add_prefixes bar ()
 
@@ -143,6 +138,51 @@ corresponding `Quantity` object with `Float64` value. For example,
 """
 macro u_str(s)
     return esc(uparse(s))
+end
+
+"""
+    UnitSymbols
+
+A separate module where each unit is treated as a separate dimension,
+to enable pretty-printing of units.
+"""
+module UnitSymbols
+
+    import ...Quantity
+    import ...AbstractDimensions
+    import ...DEFAULT_VALUE_TYPE
+    import ...DEFAULT_DIM_BASE_TYPE
+
+    import .._UNIT_SYMBOLS
+
+    """A tuple of all possible unit symbols."""
+    const UNIT_SYMBOLS = Tuple(_UNIT_SYMBOLS)
+
+    function _create_unit_dimensions()
+        struct_def = :(struct UnitDimensions{R} <: AbstractDimensions{R}; end)
+        fields = struct_def.args[3].args
+        for unit in UNIT_SYMBOLS
+            push!(fields, :($(unit)::R))
+        end
+        return struct_def
+    end
+
+    # Create an AbstractDimensions containing all symbols:
+    @eval $(_create_unit_dimensions())
+
+    # Create all unit symbols
+    for unit in UNIT_SYMBOLS
+        @eval const $unit = Quantity(1.0, UnitDimensions, $(unit)=1)
+    end
+
+    function uparse(s::AbstractString)
+        return as_quantity(eval(Meta.parse(s)))::Quantity{DEFAULT_VALUE_TYPE,UnitDimensions{DEFAULT_DIM_BASE_TYPE}}
+    end
+
+    as_quantity(q::Quantity) = q
+    as_quantity(x::Number) = Quantity(convert(DEFAULT_VALUE_TYPE, x), UnitDimensions{DEFAULT_DIM_BASE_TYPE})
+    as_quantity(x) = error("Unexpected type evaluated: $(typeof(x))")
+
 end
 
 end
