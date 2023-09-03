51d83bf7dca64bb08bfdbfeead2ad56a1310a185
MilesCranmer <miles.cranmer@gmail.com>
2023-09-03 14:30:59 -0400

    Refactor benchmark to be more hierarchical

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,7 +3,9 @@ using DynamicQuantities
 
 const SUITE = BenchmarkGroup()
 
-SUITE["creation"] = let s = BenchmarkGroup()
+SUITE["Quantity"] = BenchmarkGroup()
+
+SUITE["Quantity"]["creation"] = let s = BenchmarkGroup()
     s["Quantity(x)"] = @benchmarkable Quantity(x) setup = (x = randn()) evals = 1000
     s["Quantity(x, length=y)"] = @benchmarkable Quantity(x, length=y) setup = (x = randn(); y = rand(1:5)) evals = 1000
     s
@@ -11,7 +13,7 @@ end
 
 default() = Quantity(rand(), length=rand(1:5), mass=rand(1:5) // 2)
 
-SUITE["with_numbers"] = let s = BenchmarkGroup()
+SUITE["Quantity"]["with_numbers"] = let s = BenchmarkGroup()
     f1(x, i) = x^i
     s["^int"] = @benchmarkable $f1(x, i) setup = (x = default(); i = rand(1:5)) evals = 1000
     f2(x, y) = x * y
@@ -21,7 +23,7 @@ SUITE["with_numbers"] = let s = BenchmarkGroup()
     s
 end
 
-SUITE["with_self"] = let s = BenchmarkGroup()
+SUITE["Quantity"]["with_self"] = let s = BenchmarkGroup()
     f4(x) = inv(x)
     s["inv"] = @benchmarkable $f4(x) setup = (x = default()) evals = 1000
     f7(x) = ustrip(x)
@@ -31,7 +33,7 @@ SUITE["with_self"] = let s = BenchmarkGroup()
     s
 end
 
-SUITE["with_quantity"] = let s = BenchmarkGroup()
+SUITE["Quantity"]["with_quantity"] = let s = BenchmarkGroup()
     f5(x, y) = x / y
     s["/y"] = @benchmarkable $f5(x, y) setup = (x = default(); y = default()) evals = 1000
     f6(x, y) = x + y
@@ -39,15 +41,17 @@ SUITE["with_quantity"] = let s = BenchmarkGroup()
     s
 end
 
-SUITE["with_array"] = let s = BenchmarkGroup()
+SUITE["QuantityArray"] = BenchmarkGroup()
+
+SUITE["QuantityArray"]["broadcasting"] = let s = BenchmarkGroup()
     N = 10000
     f9(x) = x^2
-    s["arr.^2"] = @benchmarkable $f9.(arr) setup = (arr = randn($N)) evals = 1000 # baseline
-    s["qarr.^2"] = @benchmarkable $f9.(arr) setup = (arr = QuantityArray(randn($N), u"km/s")) evals = 1000
-    s["arrq.^2"] = @benchmarkable $f9.(arr) setup = (arr = randn($N) .* u"km/s") evals = 1000
+    s["pow2_normal_array"] = @benchmarkable $f9.(arr) setup = (arr = randn($N)) evals = 1000 # baseline
+    s["pow2_quantity_array"] = @benchmarkable $f9.(arr) setup = (arr = QuantityArray(randn($N), u"km/s")) evals = 1000
+    s["pow2_array_of_quantities"] = @benchmarkable $f9.(arr) setup = (arr = randn($N) .* u"km/s") evals = 1000
     f10(x) = x^4
-    s["arr.^4"] = @benchmarkable $f9.(arr) setup = (arr = randn($N)) evals = 1000 # baseline
-    s["qarr.^4"] = @benchmarkable $f9.(arr) setup = (arr = QuantityArray(randn($N), u"km/s")) evals = 1000
-    s["arrq.^4"] = @benchmarkable $f9.(arr) setup = (arr = randn($N) .* u"km/s") evals = 1000
+    s["pow4_normal_array"] = @benchmarkable $f9.(arr) setup = (arr = randn($N)) evals = 1000 # baseline
+    s["pow4_quantity_array"] = @benchmarkable $f9.(arr) setup = (arr = QuantityArray(randn($N), u"km/s")) evals = 1000
+    s["pow4_array_of_quantities"] = @benchmarkable $f9.(arr) setup = (arr = randn($N) .* u"km/s") evals = 1000
     s
 end
