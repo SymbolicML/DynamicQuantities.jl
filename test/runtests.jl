303c25b7e79e9b583654571f55ab3015196e40b5
MilesCranmer <miles.cranmer@gmail.com>
2023-09-19 13:33:14 +0100

    HACK - only test quadgk

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,6 @@
 using SafeTestsets
 import Ratios: SimpleRatio
 
-@static if !hasmethod(round, Tuple{Int, SimpleRatio{Int}})
-    @eval Base.round(T, x::SimpleRatio) = round(T, x.num // x.den)
-end
-
-if parse(Bool, get(ENV, "DQ_TEST_UPREFERRED", "false"))
-    @safetestset "Test upreferred disallowed" begin
-        include("test_ban_upreferred.jl")
-    end
-else
-    @safetestset "Unitful.jl integration tests" begin
-        include("test_unitful.jl")
-    end
-    @safetestset "ScientificTypes.jl integration tests" begin
-        include("test_scitypes.jl")
-    end
-    @safetestset "Unit tests" begin
-        include("unittests.jl")
-    end
-    @safetestset "Aqua tests" begin
-        include("test_aqua.jl")
-    end
-    @safetestset "QuadGK integration tests" begin
-        include("test_quadgk.jl")
-    end
+@safetestset "QuadGK integration tests" begin
+    include("test_quadgk.jl")
 end
