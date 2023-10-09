d2ea3534f19f06fd5105f4d5517cbfc5f2f3d817
David Widmann <devmotion@users.noreply.github.com>
2023-10-10 00:09:09 +0200

    Update Aqua tests

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,4 +1,9 @@
 using DynamicQuantities
 using Aqua
 
-Aqua.test_all(DynamicQuantities; project_toml_formatting=false)
+Aqua.test_all(
+    DynamicQuantities;
+    project_toml_formatting=false,
+    # Requires is only loaded on older Julia versions that do not support extensions
+    stale_deps=(ignore=isdefined(Base, :get_extension) ? [:Requires] : Symbol[],),
+)
