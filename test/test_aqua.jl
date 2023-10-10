8cc4fe72931a3fb1ec24581ca89c2b2bd1ee570c
David Widmann <devmotion@noreply.users.github.com>
2023-10-10 16:28:57 +0200

    Revert Aqua test

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,9 +1,4 @@
 using DynamicQuantities
 using Aqua
 
-Aqua.test_all(
-    DynamicQuantities;
-    project_toml_formatting=false,
-    # Requires is only loaded on older Julia versions that do not support extensions
-    stale_deps=(ignore=isdefined(Base, :get_extension) ? [:Requires] : Symbol[],),
-)
+Aqua.test_all(DynamicQuantities; project_toml_formatting=false)
