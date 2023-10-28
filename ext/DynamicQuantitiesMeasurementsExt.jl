ce9d527195f7edfb642adf0ad3e64e9a4972e94d
MilesCranmer <miles.cranmer@gmail.com>
2023-10-28 23:14:59 +0100

    Add compatibility for generic quantities in measurements

--- ext/DynamicQuantitiesMeasurementsExt.jl
+++ ext/DynamicQuantitiesMeasurementsExt.jl
@@ -1,18 +1,18 @@
 module DynamicQuantitiesMeasurementsExt
 
-using DynamicQuantities: AbstractQuantity, new_quantity, dimension, ustrip, DimensionError
+using DynamicQuantities: AbstractUnionQuantity, new_quantity, dimension, ustrip, DimensionError
 using Measurements: Measurements, measurement, value, uncertainty
 
-function Measurements.measurement(a::Q, b::Q) where {Q<:AbstractQuantity}
+function Measurements.measurement(a::Q, b::Q) where {Q<:AbstractUnionQuantity}
     dimension(a) == dimension(b) || throw(DimensionError(a, b))
     raw_measurement = measurement(ustrip(a), ustrip(b))
     return new_quantity(Q, raw_measurement, dimension(a))
 end
-function Measurements.measurement(a::AbstractQuantity, b::AbstractQuantity)
+function Measurements.measurement(a::AbstractUnionQuantity, b::AbstractUnionQuantity)
     return measurement(promote(a, b)...)
 end
 
-Measurements.value(q::Q) where {Q<:AbstractQuantity} = new_quantity(Q, value(ustrip(q)), dimension(q))
-Measurements.uncertainty(q::Q) where {Q<:AbstractQuantity} = new_quantity(Q, uncertainty(ustrip(q)), dimension(q))
+Measurements.value(q::Q) where {Q<:AbstractUnionQuantity} = new_quantity(Q, value(ustrip(q)), dimension(q))
+Measurements.uncertainty(q::Q) where {Q<:AbstractUnionQuantity} = new_quantity(Q, uncertainty(ustrip(q)), dimension(q))
 
 end
