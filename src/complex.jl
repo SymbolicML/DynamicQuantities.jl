fa076eaecc61192fc20dc0ee97a3f5aee7bcf038
MilesCranmer <miles.cranmer@gmail.com>
2023-11-30 13:14:38 +0000

    Move complex overloads back to disambiguities

--- src/complex.jl
+++ src/complex.jl
@@ -20,20 +20,3 @@ for (type, true_base_type, _) in ABSTRACT_QUANTITY_TYPES
         @eval Base.complex(a::$type, b::$type2) = complex(promote_except_value(a, b)...)
     end
 end
-
-for type in (:(Complex), :(Complex{Bool}))
-    @eval begin
-        function Base.:*(l::$type, r::AbstractRealQuantity)
-            new_quantity(typeof(r), l * ustrip(r), dimension(r))
-        end
-        function Base.:*(l::AbstractRealQuantity, r::$type)
-            new_quantity(typeof(l), ustrip(l) * r, dimension(l))
-        end
-    end
-end
-function Base.:/(l::Complex, r::AbstractRealQuantity)
-    new_quantity(typeof(r), l / ustrip(r), inv(dimension(r)))
-end
-function Base.:/(l::AbstractRealQuantity, r::Complex)
-    new_quantity(typeof(l), ustrip(l) / r, dimension(l))
-end
