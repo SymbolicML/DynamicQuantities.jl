64a1cbed012cc9c76350a93c34608a4aa4a52f86
MilesCranmer <miles.cranmer@gmail.com>
2024-03-02 21:39:36 +0000

    Make `uconvert` tests safer

--- test/unittests.jl
+++ test/unittests.jl
@@ -799,8 +799,9 @@ end
     @test dimension(qs)[:M_sun] == 1
     @test uexpand(qs) ≈ 5.0 * q
 
-    @test uconvert(us"nm",1u"m") == 1u"m" |> us"nm"
-    
+    @test dimension(1u"m" |> us"nm")[:nm] == 1
+    @test dimension(1u"m" |> us"nm")[:m] == 0
+
     # Refuses to convert to non-unit quantities:
     @test_throws AssertionError uconvert(1.2us"m", 1.0u"m")
     VERSION >= v"1.8" &&
@@ -824,25 +825,30 @@ end
         xs2 = x2 .|> us"g"
         @test typeof(xs2) <: Vector{<:Q{Float64,<:SymbolicDimensions{<:Any}}}
         @test xs2[2] ≈ Q(2000us"g")
+        @test ustrip(xs2[2]) ≈ 2000
         
         x_qa = QuantityArray(x)
         xs_qa = x_qa .|> uconvert(us"g")
         @test typeof(xs_qa) <: QuantityArray{Float64,1,<:SymbolicDimensions{<:Any}}
         @test xs_qa[2] ≈ Q(2000us"g")
+        @test ustrip(xs_qa[1]) ≈ 1000
 
         x_qa1 = QuantityArray(x)
         xs_qa1 = x_qa1 .|> us"g"
         @test typeof(xs_qa1) <: QuantityArray{Float64,1,<:SymbolicDimensions{<:Any}}
         @test xs_qa1[2] ≈ Q(2000us"g")
+        @test ustrip(xs_qa1[3]) ≈ 3000
 
         # Without vectorized call:
         xs_qa2 = x_qa |> uconvert(us"g")
         @test typeof(xs_qa2) <: QuantityArray{Float64,1,<:SymbolicDimensions{<:Any}}
         @test xs_qa2[2] ≈ Q(2000us"g")
+        @test ustrip(xs_qa2[2]) ≈ 2000
 
         xs_qa3 = x_qa |> us"g"
         @test typeof(xs_qa3) <: QuantityArray{Float64,1,<:SymbolicDimensions{<:Any}}
         @test xs_qa3[2] ≈ Q(2000us"g")
+        @test ustrip(xs_qa3[3]) ≈ 3000
     end
 end
 
