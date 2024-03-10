50a9c5c7ca1da4de3a3c55456ec50a2b6c49d5f3
Michael Ingold <mike.ingold@gmail.com>
2024-03-10 17:46:01 -0400

    Bugfix - QuantityArray not in namespace

--- ext/DynamicQuantitiesLinearAlgebraExt.jl
+++ ext/DynamicQuantitiesLinearAlgebraExt.jl
@@ -1,7 +1,7 @@
 module DynamicQuantitiesLinearAlgebraExt
 
 import LinearAlgebra: norm
-import DynamicQuantities: UnionAbstractQuantity, ustrip, dimension, new_quantity
+import DynamicQuantities: UnionAbstractQuantity, QuantityArray, ustrip, dimension, new_quantity
 
 norm(q::UnionAbstractQuantity, p::Real=2) = new_quantity(typeof(q), norm(ustrip(q), p), dimension(q))
 
