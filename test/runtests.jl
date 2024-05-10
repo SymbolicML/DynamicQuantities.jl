442a67ee67c6bc9b525c4cfc67dd425c3af6acf9
MilesCranmer <miles.cranmer@gmail.com>
2024-05-10 12:21:06 +0100

    test: convert to testitem

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using SafeTestsets
+using TestItems: @testitem
 using TestItemRunner
 import Ratios: SimpleRatio
 
@@ -6,32 +6,34 @@ if !hasmethod(round, Tuple{Int, SimpleRatio{Int}})
     Base.round(::Type{T}, x::SimpleRatio) where {T} = round(T, x.num // x.den)
 end
 
+@testset "Test upreferred disallowed" tags=[:upreferred] begin
+    include("test_ban_upreferred.jl")
+end
+
+@testitem "Unitful.jl integration tests" begin
+    include("test_unitful.jl")
+end
+@testitem "ScientificTypes.jl integration tests" begin
+    include("test_scitypes.jl")
+end
+@testitem "Measurements.jl integration tests" begin
+    include("test_measurements.jl")
+end
+## Broken; see https://github.com/SymbolicML/DynamicQuantities.jl/issues/118
+# @testitem "Meshes.jl integration tests" begin
+#     include("test_meshes.jl")
+# end
+include("unittests.jl")
+
+@testitem "LinearAlgebra.jl integration tests" begin
+    include("test_linearalgebra.jl")
+end
+@testitem "Aqua tests" begin
+    include("test_aqua.jl")
+end
+
 if parse(Bool, get(ENV, "DQ_TEST_UPREFERRED", "false"))
-    @safetestset "Test upreferred disallowed" begin
-        include("test_ban_upreferred.jl")
-    end
+    @eval @run_package_tests filter=t -> :upreferred in t.tags
 else
-    @safetestset "Unitful.jl integration tests" begin
-        include("test_unitful.jl")
-    end
-    @safetestset "ScientificTypes.jl integration tests" begin
-        include("test_scitypes.jl")
-    end
-    @safetestset "Measurements.jl integration tests" begin
-        include("test_measurements.jl")
-    end
-    ## Broken; see https://github.com/SymbolicML/DynamicQuantities.jl/issues/118
-    # @safetestset "Meshes.jl integration tests" begin
-    #     include("test_meshes.jl")
-    # end
-    @safetestset "Unit tests" begin
-        include("unittests.jl")
-    end
-    @safetestset "LinearAlgebra.jl integration tests" begin
-        include("test_linearalgebra.jl")
-    end
-    @safetestset "Aqua tests" begin
-        include("test_aqua.jl")
-    end
-    @eval @run_test_items
+    @eval @run_package_tests filter=t -> !(:upreferred in t.tags)
 end
--- test/test_linearalgebra.jl
+++ test/test_linearalgebra.jl
@@ -1,80 +1,52 @@
-using DynamicQuantities, LinearAlgebra
+# using DynamicQuantities, LinearAlgebra
 
-# checks for unit consistency with subtraction
-within(A, B, tol) = ustrip(maximum(abs.(ustrip(A - B)))) < tol
+# # check ldiv with 2D x 2D `QuantityArray`s
+# D = A * C
+# @test within(A \ D, C, 1e-12)
 
-v = randn(2, 2)
-d = 1u"m"
-e = u"m"
-A = QuantityArray(v, d) # Create a `QuantityArray` with value `value` and dimensions `dimensions`.
+# F = svd(A)
+# U, σ, V = F
 
-# vector multiplication
-q = [1, 2]
-r = A \ q
-@test isequal(dimension(A * r), dimension(QuantityArray(q)))
-@test within(ustrip(A * r), q, 1e-10)
+# inv(F) # doesn't return a QuantityArray, could add a wrapper here
 
-# ldiv 2D QuantityArray and 1D QuantityArray
-g = QuantityArray(randn(2), d)
-h = A \ g
-@test within(A * h, g, 1e-10)
+# @test isequal(dimension(σ), dimension(A))
 
-# test inv
-B = inv(A)
-@test isapprox(B * A, I(2), rtol=1e-10)
-@test isapprox(A * B, I(2), rtol=1e-10)
+# Σ = Diagonal(σ)
+# @test within(U * Σ * V', A, 1e-10)
 
-vc = randn(2, 2)
-dc = 1u"kg"
-C = QuantityArray(vc, dc) # Create a `QuantityArray` with v
+# # test that diagm runs
+# Σwide = diagm(2, 3, σ)
+# Σsquare = diagm(σ)
 
-# check ldiv with 2D x 2D `QuantityArray`s
-D = A * C
-@test within(A \ D, C, 1e-12)
+# # SVD in modal form
+# # truncated SVD
+# K = 2
+# U[:, 1:K] * Diagonal(σ[1:K]) * V[:, 1:K]'
 
-F = svd(A)
-U, σ, V = F
+# K = 1
+# U[:, 1:K] * Diagonal(σ[1:K]) * V[:, 1:K]'
 
-inv(F) # doesn't return a QuantityArray, could add a wrapper here
+# # retain QuantityArray, best way to store this info
+# @test Σ * Σ isa QuantityArray
+# @test U * Σ isa QuantityArray
+# @test U' * Σ isa QuantityArray
+# @test transpose(U) * Σ isa QuantityArray
+# @test Σ * V isa QuantityArray
+# @test Σ * V' isa QuantityArray
+# @test Σ * transpose(V) isa QuantityArray
+# @test U * Σ * V' isa QuantityArray
 
-@test isequal(dimension(σ), dimension(A))
+# F = eigen(A)
+# vals, vecs = F
+# @test isequal(dimension(vals), dimension(A))
+# # inv(F) # doesn't work
+# @test within(det(F), det(A), 1e-10)
 
-Σ = Diagonal(σ)
-@test within(U * Σ * V', A, 1e-10)
+# # eigenstructure satisfied?
+# @test within(A * vecs, vecs * Diagonal(vals), 1e-10)
 
-# test that diagm runs
-Σwide = diagm(2, 3, σ)
-Σsquare = diagm(σ)
+# det(A) # does it run?
 
-# SVD in modal form
-# truncated SVD
-K = 2
-U[:, 1:K] * Diagonal(σ[1:K]) * V[:, 1:K]'
+# # svdvals(A) # error, LinearAlgebra.eigen.jl doesn't handle it
 
-K = 1
-U[:, 1:K] * Diagonal(σ[1:K]) * V[:, 1:K]'
-
-# retain QuantityArray, best way to store this info
-@test Σ * Σ isa QuantityArray
-@test U * Σ isa QuantityArray
-@test U' * Σ isa QuantityArray
-@test transpose(U) * Σ isa QuantityArray
-@test Σ * V isa QuantityArray
-@test Σ * V' isa QuantityArray
-@test Σ * transpose(V) isa QuantityArray
-@test U * Σ * V' isa QuantityArray
-
-F = eigen(A)
-vals, vecs = F
-@test isequal(dimension(vals), dimension(A))
-# inv(F) # doesn't work
-@test within(det(F), det(A), 1e-10)
-
-# eigenstructure satisfied?
-@test within(A * vecs, vecs * Diagonal(vals), 1e-10)
-
-det(A) # does it run?
-
-# svdvals(A) # error, LinearAlgebra.eigen.jl doesn't handle it
-
-# isapprox(ustrip(transpose(A)),transpose(ustrip(A))) # no show method for Transpose{QuantityArray}
+# # isapprox(ustrip(transpose(A)),transpose(ustrip(A))) # no show method for Transpose{QuantityArray}
