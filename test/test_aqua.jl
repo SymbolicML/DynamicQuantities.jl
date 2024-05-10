027f920de6e1e6b6d3f9df38f17745f5236e9ea3
MilesCranmer <miles.cranmer@gmail.com>
2024-05-10 23:13:16 +0100

    test: skip ambiguities tests on Julia other than 1.10

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,4 +1,9 @@
 using DynamicQuantities
 using Aqua
 
-Aqua.test_all(DynamicQuantities)
+Aqua.test_all(DynamicQuantities; ambiguities=false)
+
+# Only test range of versions so we don't go insane with ambiguities
+@static if VERSION >= v"1.10" && VERSION < v"1.11"
+    Aqua.test_ambiguities(DynamicQuantities)
+end
