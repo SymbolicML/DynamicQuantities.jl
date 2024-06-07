972edf4fc10cf791e2a769a280c0c7be4c387ea8
Ruben Gonzalez <40864154+Deduction42@users.noreply.github.com>
2024-06-06 22:20:19 -0600

    Performance improvements for Unitful conversion (#141)

--- ext/DynamicQuantitiesUnitfulExt.jl
+++ ext/DynamicQuantitiesUnitfulExt.jl
@@ -9,11 +9,11 @@ function get_si_units()
     return (length=u"m", mass=u"kg", time=u"s", current=u"A", temperature=u"K", luminosity=u"cd", amount=u"mol")
 end
 
-function validate_upreferred()
+@generated function validate_upreferred()
     si_units = get_si_units()
     for k in keys(si_units)
         if Unitful.upreferred(si_units[k]) !== si_units[k]
-            error("Found custom `Unitful.preferunits`. This is not supported when interfacing Unitful and DynamicQuantities: you must leave the default `Unitful.upreferred`, which is the SI base units.")
+            return :(error("Found custom `Unitful.preferunits`. This is not supported when interfacing Unitful and DynamicQuantities: you must leave the default `Unitful.upreferred`, which is the SI base units."))
         end
     end
     return true
@@ -53,29 +53,22 @@ for (_, _, Q) in ABSTRACT_QUANTITY_TYPES
 end
 
 Base.convert(::Type{DynamicQuantities.Dimensions}, d::Unitful.Dimensions) = convert(DynamicQuantities.DEFAULT_DIM_TYPE, d)
-Base.convert(::Type{DynamicQuantities.Dimensions{R}}, d::Unitful.Dimensions{D}) where {R,D} =
-    let
-        validate_upreferred()
-        cumulator = DynamicQuantities.Dimensions{R}()
-        for dim in D
-            dim_symbol = _map_dim_name_to_dynamic_units(typeof(dim))
-            dim_power = dim.power
-            cumulator *= DynamicQuantities.Dimensions(R; dim_symbol => dim_power)
-        end
-        cumulator
-    end
 
-function _map_dim_name_to_dynamic_units(::Type{Unitful.Dimension{D}}) where {D}
-    # (We could do this automatically, but it's more obvious what we are doing this way.)
-    D == :Length && return :length
-    D == :Mass && return :mass
-    D == :Time && return :time
-    D == :Current && return :current
-    D == :Temperature && return :temperature
-    D == :Luminosity && return :luminosity
-    D == :Amount && return :amount
-    error("Unknown dimension: $D")
+function Base.convert(::Type{DynamicQuantities.Dimensions{R}}, d::Unitful.Dimensions{D}) where {R,D}
+    validate_upreferred()
+    return prod(Base.Fix1(_dynamic_dimension, R), D)
 end
 
+function _dynamic_dimension(::Type{R}, dims::Unitful.Dimension{D}) where {R,D}
+    DimType = DynamicQuantities.Dimensions{R}
+    D == :Length && return DimType(length=dims.power)
+    D == :Mass && return DimType(mass=dims.power)
+    D == :Time && return DimType(time=dims.power)
+    D == :Current && return DimType(current=dims.power)
+    D == :Temperature && return DimType(temperature=dims.power)
+    D == :Luminosity && return DimType(luminosity=dims.power)
+    D == :Amount && return DimType(amount=dims.power)
+    error("Unknown dimension: $D")
+end
 
 end
