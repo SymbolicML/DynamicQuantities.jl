62b4ed004ec3aca79470fd0d543e53fc1040febf
MilesCranmer <miles.cranmer@gmail.com>
2024-11-01 13:27:09 +0000

    test: disable stale deps test on 1.11

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,9 +1,9 @@
 using DynamicQuantities
 using Aqua
 
-Aqua.test_all(DynamicQuantities; ambiguities=false)
+Aqua.test_all(DynamicQuantities; ambiguities=false, stale_deps=VERSION < v"1.11.0-")
 
 # Only test range of versions so we don't go insane with ambiguities
-@static if VERSION >= v"1.10" && VERSION < v"1.11"
+@static if VERSION >= v"1.10.0-" && VERSION < v"1.11.0-"
     Aqua.test_ambiguities(DynamicQuantities)
 end
